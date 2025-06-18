b4956c814ee5c77cc96aaec8ef5e95add847a375
Josh Buchanan <48423110+jbuchanan00@users.noreply.github.com>
2025-06-17 21:40:46 -0600

    added adhoc for database hydration

--- server/cmd/adhoc/processGeoData/main.go
+++ server/cmd/adhoc/processGeoData/main.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	config "halo/cmd/adhoc/infrastructure"
+	"io"
 	"log"
+	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
+
+type Coordinates struct {
+	Lon float32 `json:"lon"`
+	Lat float32 `json:"lat"`
+}
+
+type Location struct {
+	Id      float32     `json:"id"`
+	Name    string      `json:"name"`
+	State   string      `json:"state"`
+	Country string      `json:"country"`
+	Coord   Coordinates `json:"coord"`
+}
 
 //lint:ignore U1000 this is a placeholder main function
 func main() {
@@ -16,11 +32,43 @@ func main() {
 	}
 	defer dbpool.Close()
 
-	var greeting string
-	err = dbpool.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	file, err := os.Open("./city.list.json")
 	if err != nil {
-		log.Printf("QueryRow failed: %v\n", err)
+		log.Printf("Unable to read file")
 	}
 
-	log.Println(greeting)
+	bytevalue, err := io.ReadAll(file)
+	if err != nil {
+		log.Printf("Unable to parse file")
+	}
+
+	var locations []Location
+
+	if err = json.Unmarshal(bytevalue, &locations); err != nil {
+		log.Printf("Unable to unmarshal file %s", err)
+	}
+
+	createTables(dbpool)
+
+	for _, item := range locations {
+		insertLocation(dbpool, item)
+		log.Printf("Item: %d, %s, %v", int(item.Id), item.Name, item.Coord.Lat)
+	}
+
+	log.Println("Completed")
+}
+
+func createTables(db *pgxpool.Pool) {
+	_, err := db.Exec(context.Background(), "CREATE TABLE IF NOT EXISTS location (id INTEGER, name VARCHAR, state VARCHAR, country VARCHAR,latitude decimal, longitude decimal)")
+	if err != nil {
+		log.Printf("Error creating the tables %s", err)
+	}
+}
+
+func insertLocation(db *pgxpool.Pool, item Location) {
+	_, err := db.Exec(context.Background(), "INSERT INTO location (id, name, state, country, latitude, longitude) VALUES ($1, $2, $3, $4, $5, $6)", int(item.Id), item.Name, item.State, item.Country, item.Coord.Lat, item.Coord.Lon)
+	if err != nil {
+		log.Printf("Error inserting %s", item.Name)
+	}
+	log.Printf("Inserting")
 }
